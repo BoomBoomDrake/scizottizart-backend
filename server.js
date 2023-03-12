1a0bdc4608e37557322c4bb76c630c8a7ef736d5
Eric Drake <eric.drake2011@gmail.com>
2023-03-12 14:14:56 -0500

    changed cors origin for frontend testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5000"}));
+app.use(cors({origin: "http://localhost:3000"}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
