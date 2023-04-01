4eec6e364725abeab9bf89ee8e9689a3d14fafdb
Eric Drake <eric.drake2011@gmail.com>
2023-04-01 11:21:24 -0500

    added fixie-socks proxy info

--- index.js
+++ index.js
@@ -6,21 +6,51 @@ dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000;
+const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[/(:\\/@/]+'));
 
-MongoClient.connect(
-    process.env.STOREITEMS_DB_URI,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500,
-    }
-)
-.catch(err => {
-    console.log(err.stack);
-    process.exit(1);
-})
-.then(async client => {
-    await StoreItemDAO.injectDB(client);
-    app.listen(port, () => {
-        console.log(`Listening on port ${port}.`);
+const environment = process.env.NODE_ENV;
+
+if (environment === "test") {
+    MongoClient.connect(
+        process.env.STOREITEMS_DB_URI,
+        {
+            maxPoolSize: 50,
+            wtimeoutMS: 2500,
+        }
+    )
+    .catch(err => {
+        console.log(err.stack);
+        process.exit(1);
+    })
+    .then(async client => {
+        await StoreItemDAO.injectDB(client);
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}.`);
+        });
     });
-});
+} else if (environment === "production") {
+    MongoClient.connect(
+        process.env.STOREITEMS_DB_URI,
+        {
+            maxPoolSize: 50,
+            wtimeoutMS: 2500,
+            proxyUsername: fixieData[0],
+            proxyPassword: fixieData[1],
+            proxyHost: fixieData[2],
+            proxyPort: fixieData[3]
+        }
+    )
+    .catch(err => {
+        console.log(err.stack);
+        process.exit(1);
+    })
+    .then(async client => {
+        await StoreItemDAO.injectDB(client);
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}.`);
+        });
+    });
+} else {
+    console.log("Error with node environment variable");
+}
+
