9eae280537445cf9775e1aa40b5c325ae7250910
Eric Drake <eric.drake2011@gmail.com>
2023-04-01 11:35:10 -0500

    changed cors origin to production site url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:3000" }));
+app.use(cors({origin: "https://main--leafy-fox-3f5b95.netlify.app/" }));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
